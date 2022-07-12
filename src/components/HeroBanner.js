02787d394ff4be36f97aba0c0fe4e616d883ae6d
BhagyeshP1 <pbhagyesh973@gmail.com>
2022-07-11 22:37:36 -0400

    Worked on Search Bar

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -9,7 +9,7 @@ const HeroBanner = () => {
         mt: {lg: '212px', xs:'70px'},           // This helps us style the boxes on speific sizes devices //
         ml: {sm: '50px'}
     }} position='relative' p='20px' >
-        <Typography color="#FF2625"
+        <Typography color="#3CAEA3"
         fontWeight='600'
         fontSize='26px' >
             Fitness Club
@@ -17,15 +17,30 @@ const HeroBanner = () => {
         <Typography fontWeight={700}
         sx={{ fontSize: { lg: '45px', xs: '41px'}
         }}
+        mb="23px" mt="30px"
         >
             Sweat, Smile <br /> and Repeat
         </Typography>
-        <Typography fontSize='22px' lineHeight='35px' mb={3} >
+        <Typography fontSize='22px' lineHeight='35px' mb={4} >
             Check out the most effective exercises
         </Typography>
-        <Button variant='contained' color='secondary' href='#exercises' >  {/* This href will scroll down the page */ }
+        <Button variant='contained' color='error' href='#exercises'   // This href will scroll down the page //
+        sx={{ backgroundColor: '#3CAEA3'
+            
+        }}
+         >  
             Explore Exercises
         </Button>
+        <Typography fontWeight={600}
+        color="#3CAEA3"
+        sx={{
+            opacity: 0.15,
+            display: { lg: 'block', xs: 'none'}
+        }}
+        fontSize="200px"
+        >
+            Exercise
+        </Typography>
         <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
     </Box>
   )
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems='center' mt='37px'
+    justifyContent='center' p='20px'
+     >
+    <Typography 
+    fontWeight={600}
+    sx={{
+        fontSize: { lg:'44px', xs:'30px' }}}
+    mb="50px"
+    textAlign='center'
+    >
+        Awesome Exercises <br /> You Should Know
+    </Typography>
+    <Box posito="relative" mb='72px'>
+        <TextField 
+            height='76px'
+            value=''
+            onChange={(e) => {}}
+            placeholder='Search Exercises'
+            type='text'
+        />
+
+    </Box>
+    </Stack>
   )
 }
 
